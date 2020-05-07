0140bc4770df5fe0553388a4da84fd01337b7e67
Patrick O'Grady <prohb125@gmail.com>
2020-05-06 17:59:12 -0700

    remove json.RawMessage

--- types/block.go
+++ types/block.go
@@ -16,15 +16,13 @@
 
 package types
 
-import "encoding/json"
-
 // Block Blocks contain an array of Transactions that occurred at a particular BlockIdentifier.
 type Block struct {
 	BlockIdentifier       *BlockIdentifier `json:"block_identifier"`
 	ParentBlockIdentifier *BlockIdentifier `json:"parent_block_identifier"`
 	// The timestamp of the block in milliseconds since the Unix Epoch. The timestamp is stored in
 	// milliseconds because some blockchains produce blocks more often than once a second.
-	Timestamp    int64           `json:"timestamp"`
-	Transactions []*Transaction  `json:"transactions"`
-	Metadata     json.RawMessage `json:"metadata,omitempty"`
+	Timestamp    int64                  `json:"timestamp"`
+	Transactions []*Transaction         `json:"transactions"`
+	Metadata     map[string]interface{} `json:"metadata,omitempty"`
 }
