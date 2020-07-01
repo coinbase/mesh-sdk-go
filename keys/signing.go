33aea5126671d253996b135da5cda20288a4cd81
Julian <julian.koh@coinbase.com>
2020-07-01 15:26:39 +0100

    use rosetta construction spec

--- keys/signing.go
+++ keys/signing.go
@@ -18,20 +18,23 @@ import (
 	"crypto/ed25519"
 	"encoding/hex"
 	"fmt"
-	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/coinbase/rosetta-sdk-go/asserter"
+	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
-func signECDSA(privKeyBytes, payload []byte, signatureType SignatureType) ([]byte, error) {
+func signECDSA(privKeyBytes, payload []byte, signatureType types.SignatureType) ([]byte, error) {
 	var sig []byte
 	var err error
 
-	if signatureType.IsEcdsaPubkeyRecovery() {
+	switch signatureType {
+	case types.EcdsaRecovery:
 		sig, err = secp256k1.Sign(payload, privKeyBytes)
 		if err != nil {
 			return nil, fmt.Errorf("sign: unable to sign. %w", err)
 		}
-	} else {
+	case types.Ecdsa:
 		sig, err = secp256k1.Sign(payload, privKeyBytes)
 		if err != nil {
 			return nil, fmt.Errorf("sign: unable to sign. %w", err)
@@ -48,39 +51,39 @@ func signEd25519(privKeyBytes, payload []byte) []byte {
 	return sig
 }
 
-func SignPayload(payload *SigningPayload, keypair *KeyPair) (*Signature, error) {
-	privKeyBytes, err := hex.DecodeString(keypair.PrivateKey.PrivKeyHex)
+func SignPayload(payload *types.SigningPayload, keypair *KeyPair) (*types.Signature, error) {
+	privKeyBytes, err := hex.DecodeString(keypair.PrivateKey.HexBytes)
 	if err != nil {
 		return nil, fmt.Errorf("sign: unable to decode private key. %w", err)
 	}
 
-	decodedMessage, err := hex.DecodeString(payload.PayloadHex)
+	decodedMessage, err := hex.DecodeString(payload.HexBytes)
 	if err != nil {
 		return nil, fmt.Errorf("sign: unable to decode message. %w", err)
 	}
 
-	curve := keypair.PrivateKey.Curve
-	switch {
-	case curve.IsSecp256k1():
+	curve := keypair.PrivateKey.CurveType
+	switch curve {
+	case types.Secp256k1:
 		sig, err := signECDSA(privKeyBytes, decodedMessage, payload.SignatureType)
 		if err != nil {
 			return nil, err
 		}
 
-		return &Signature{
-			Payload:       payload,
-			Signature:     hex.EncodeToString(sig),
-			PublicKey:     keypair.PublicKey,
-			SignatureType: payload.SignatureType,
+		return &types.Signature{
+			SigningPayload: payload,
+			PublicKey:      keypair.PublicKey,
+			SignatureType:  payload.SignatureType,
+			HexBytes:       hex.EncodeToString(sig),
 		}, nil
-	case curve.IsEdwards25519():
+	case types.Edwards25519:
 		sig := signEd25519(privKeyBytes, decodedMessage)
 
-		return &Signature{
-			Payload:       payload,
-			Signature:     hex.EncodeToString(sig),
-			PublicKey:     keypair.PublicKey,
-			SignatureType: payload.SignatureType,
+		return &types.Signature{
+			SigningPayload: payload,
+			PublicKey:      keypair.PublicKey,
+			SignatureType:  payload.SignatureType,
+			HexBytes:       hex.EncodeToString(sig),
 		}, nil
 	default:
 		return nil, fmt.Errorf("%s is not supported", curve)
@@ -91,42 +94,43 @@ func verifyEd25519(pubKey, decodedMessage, decodedSignature []byte) bool {
 	return ed25519.Verify(pubKey, decodedMessage, decodedSignature)
 }
 
-func verifyECDSA(pubKey, decodedMessage, decodedSignature []byte) (bool, error) {
-	var normalizedSig []byte
-	switch len(decodedSignature) {
-	// ECDSA-PubkeyRecovery Signature
-	case crypto.SignatureLength:
-		normalizedSig = decodedSignature[:64]
-	// Regular ECDSA Signature
-	case crypto.RecoveryIDOffset:
-		normalizedSig = decodedSignature
-	default:
-		return false, fmt.Errorf("signature length %d is invalid", len(decodedSignature))
-	}
-
-	return secp256k1.VerifySignature(pubKey, decodedMessage, normalizedSig), nil
+func verifyECDSA(pubKey, decodedMessage, decodedSignature []byte) bool {
+	return secp256k1.VerifySignature(pubKey, decodedMessage, decodedSignature)
 }
 
-func Verify(signature *Signature) (bool, error) {
-	curve := signature.PublicKey.Curve
-	pubKey, err := hex.DecodeString(signature.PublicKey.PubKeyHex)
+func verifyECDSARecovery(pubKey, decodedMessage, decodedSignature []byte) bool {
+	normalizedSig := decodedSignature[:64]
+	return secp256k1.VerifySignature(pubKey, decodedMessage, normalizedSig)
+}
+
+func Verify(signature *types.Signature) (bool, error) {
+	curve := signature.PublicKey.CurveType
+	pubKey, err := hex.DecodeString(signature.PublicKey.HexBytes)
 	if err != nil {
 		return false, fmt.Errorf("verify: unable to decode pubkey. %w", err)
 	}
-	decodedMessage, err := hex.DecodeString(signature.Payload.PayloadHex)
+	decodedMessage, err := hex.DecodeString(signature.SigningPayload.HexBytes)
 	if err != nil {
 		return false, fmt.Errorf("verify: unable to decode message. %w", err)
 	}
-	decodedSignature, err := hex.DecodeString(signature.Signature)
+	decodedSignature, err := hex.DecodeString(signature.HexBytes)
 	if err != nil {
 		return false, fmt.Errorf("verify: unable to decode signature. %w", err)
 	}
 
-	switch {
-	case curve.IsSecp256k1():
-		verify, _ := verifyECDSA(pubKey, decodedMessage, decodedSignature)
+	err = asserter.Signatures([]*types.Signature{signature})
+	if err != nil {
+		return false, err
+	}
+
+	switch signature.SignatureType {
+	case types.Ecdsa:
+		verify := verifyECDSA(pubKey, decodedMessage, decodedSignature)
 		return verify, nil
-	case curve.IsEdwards25519():
+	case types.EcdsaRecovery:
+		verify := verifyECDSARecovery(pubKey, decodedMessage, decodedSignature)
+		return verify, nil
+	case types.Ed25519:
 		verify := verifyEd25519(pubKey, decodedMessage, decodedSignature)
 		return verify, nil
 	default:
--- keys/types.go
+++ keys/types.go
@@ -14,79 +14,16 @@
 
 package keys
 
-// CurveType is the type of curve associated with a PublicKey.
-type CurveType string
-
-// SignatureType is the type of a cryptographic signature.
-type SignatureType string
-
-// PublicKey contains the hex-encoded pubkey bytes as well as the CurveType
-type PublicKey struct {
-	PubKeyHex string
-	Curve     CurveType
-}
+import "github.com/coinbase/rosetta-sdk-go/types"
 
 // PrivateKey contains the hex-encoded privkey bytes as well as the CurveType
 type PrivateKey struct {
-	PrivKeyHex string
-	Curve      CurveType
+	HexBytes  string
+	CurveType types.CurveType
 }
 
 // KeyPair contains a PrivateKey and its' associated PublicKey
 type KeyPair struct {
-	PublicKey  *PublicKey
+	PublicKey  *types.PublicKey
 	PrivateKey *PrivateKey
-}
-
-// SigningPayload is signed by the client with the keypair associated
-// with an address using the specified SignatureType.
-type SigningPayload struct {
-	Address       string // in network-specific format
-	PayloadHex    string
-	SignatureType SignatureType
-}
-
-// Signature contains the payload that was signed, the public keys of the
-// keypairs used to produce the signature, the hex-encoded signature,
-// and the SignatureType.
-type Signature struct {
-	Payload *SigningPayload
-
-	// PublicKey is often times not known during construction of the signing
-	// payloads by the Wallet API Implementation but may be needed for
-	// CombineSignatures to work correctly.
-	PublicKey     *PublicKey
-	SignatureType SignatureType
-	Signature     string // in hex
-}
-
-const (
-	// Curves
-	Secp256k1Curve    = "secp256k1"    // public key is SEC compressed 33-bytes
-	Edwards25519Curve = "edwards25519" // public key is y (255-bits) || x-sign-bit (32-bytes)
-
-	// Signatures
-	EcdsaSignature               = "ecdsa"                 // r (32-bytes) || s (32-bytes)
-	EcdsaPubkeyRecoverySignature = "ecdsa_pubkey_recovery" // r (32-bytes) || s (32-bytes) || v (1-byte)
-	Ed25519Signature             = "ed25519"               // R (32-byte) || s (32-bytes)
-)
-
-func (c CurveType) IsEdwards25519() bool {
-	return c == Edwards25519Curve
-}
-
-func (c CurveType) IsSecp256k1() bool {
-	return c == Secp256k1Curve
-}
-
-func (s SignatureType) IsEcdsa() bool {
-	return s == EcdsaSignature
-}
-
-func (s SignatureType) IsEcdsaPubkeyRecovery() bool {
-	return s == EcdsaPubkeyRecoverySignature
-}
-
-func (s SignatureType) IsEd25519() bool {
-	return s == Ed25519Signature
 }
