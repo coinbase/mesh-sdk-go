356afc1aba96d991e73773e48698ca195de4f6e3
Patrick O'Grady <prohb125@gmail.com>
2020-07-07 09:32:42 -0700

    nits

--- types/marshal_test.go
+++ types/marshal_test.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Coinbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package types
 
 import (
@@ -38,4 +52,10 @@ func TestCustomMarshal(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, s, s2)
+
+	// Invalid Hex Check
+	s3 := &PublicKey{}
+	err = json.Unmarshal([]byte(`{"hex_bytes":"hello"}`), s3)
+	assert.Error(t, err)
+	assert.Len(t, s3.Bytes, 0)
 }
