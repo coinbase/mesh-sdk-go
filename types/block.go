aa50a811e96a48ebe616c79fd165981163220db6
Patrick O'Grady <prohb125@gmail.com>
2020-07-25 09:27:08 -0700

    Testing v1.4.1 ideas

--- types/block.go
+++ types/block.go
@@ -16,7 +16,11 @@
 
 package types
 
-// Block Blocks contain an array of Transactions that occurred at a particular BlockIdentifier.
+// Block Blocks contain an array of Transactions that occurred at a particular BlockIdentifier. A
+// hard requirement for blocks returned by Rosetta implementations is that they MUST be
+// _inalterable_: once a client has requested and received a block identified by a specific
+// BlockIndentifier, all future calls for that same BlockIdentifier must return the same block
+// contents.
 type Block struct {
 	BlockIdentifier       *BlockIdentifier `json:"block_identifier"`
 	ParentBlockIdentifier *BlockIdentifier `json:"parent_block_identifier"`
