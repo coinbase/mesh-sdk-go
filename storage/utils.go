051a56c7e7ec87a2f378d20963470d49a78dae9c
Patrick O'Grady <prohb125@gmail.com>
2020-08-20 18:05:41 -0700

    Compress data

--- storage/utils.go
+++ storage/utils.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/DataDog/zstd"
 	msgpack "github.com/vmihailenco/msgpack/v5"
 )
 
@@ -36,7 +37,12 @@ func encode(object interface{}) ([]byte, error) {
 		return nil, fmt.Errorf("%w: could not encode object", err)
 	}
 
-	return buf.Bytes(), nil
+	compressed, err := zstd.Compress(nil, buf.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("%w: could not compress object", err)
+	}
+
+	return compressed, nil
 }
 
 func getDecoder(r io.Reader) *msgpack.Decoder {
@@ -47,7 +53,12 @@ func getDecoder(r io.Reader) *msgpack.Decoder {
 }
 
 func decode(b []byte, object interface{}) error {
-	err := getDecoder(bytes.NewReader(b)).Decode(&object)
+	decompressed, err := zstd.Decompress(nil, b)
+	if err != nil {
+		return fmt.Errorf("%w: unable to decompress object", err)
+	}
+
+	err = getDecoder(bytes.NewReader(decompressed)).Decode(&object)
 	if err != nil {
 		return fmt.Errorf("%w: unable to decode bytes", err)
 	}
