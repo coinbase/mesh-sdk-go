2f021175e2892b190cd6cbc898f832f167775169
Patrick O'Grady <prohb125@gmail.com>
2020-10-03 16:01:54 -0700

    Progress

--- constructor/dsl/dsl.go
+++ constructor/dsl/dsl.go
@@ -2,6 +2,7 @@ package dsl
 
 import (
 	"bufio"
+	"errors"
 	"os"
 	"regexp"
 	"strings"
@@ -20,19 +21,55 @@ type parser struct {
 // TODO: if //, skip line
 
 func extractOutputPathAndType(line string) (job.ActionType, string, string, error) {
+	var outputPath string
 
+	tokens := strings.SplitN(line, "=", 1)
+	var remaining string
+	if len(tokens) == 2 {
+		outputPath = strings.TrimSpace(tokens[0])
+		remaining = tokens[1]
+	} else {
+		remaining = tokens[0]
+	}
+
+	remaining = strings.TrimSpace(remaining)
+	tokens = strings.SplitN(line, "(", 1)
+	if len(tokens) == 2 {
+		thisAction := job.ActionType(tokens[0])
+		switch thisAction {
+		case job.GenerateKey, job.Derive, job.SaveAccount, job.PrintMessage,
+			job.RandomString, job.Math, job.FindBalance, job.RandomNumber, job.Assert,
+			job.FindCurrencyAmount, job.LoadEnv, job.HTTPRequest:
+			return thisAction, outputPath, tokens[1], nil
+		default:
+			return "", "", "", errors.New("invalid action type")
+		}
+	}
+
+	if strings.HasPrefix(tokens[0], "{") {
+		if len(outputPath) > 0 {
+			return job.SetVariable, outputPath, tokens[0], nil
+		}
+
+		return "", "", "", errors.New("unable to set variable with no output path")
+	}
+
+	return "", "", "", errors.New("parsing error")
 }
 
 func (p *parser) matchAction() (*job.Action, error) {
 	var actionType job.ActionType
 	var input, outputPath string
+	var err error
 	for {
 		line := p.readLine()
 
 		// if no action type, at first line
 		if len(actionType) == 0 {
-			var err error
 			actionType, outputPath, line, err = extractOutputPathAndType(line)
+			if err != nil {
+				return nil, errors.New("unable to extract path and type")
+			}
 		}
 
 		if actionType == job.SetVariable {
