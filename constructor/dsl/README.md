53e01b74b0a96aed8aa9360add0d0a12b314ed01
Patrick O'Grady <prohb125@gmail.com>
2020-10-03 16:40:22 -0700

    chugging along

---
+++ constructor/dsl/README.md
@@ -0,0 +1,1 @@
+TODO: describe language syntax
--- constructor/dsl/errors.go
+++ constructor/dsl/errors.go
@@ -1,6 +1,15 @@
 package dsl
 
+import (
+	"errors"
+)
+
 type Error struct {
 	Line int
 	Err  error
 }
+
+var (
+	ErrEOF     = errors.New("reached end of file")
+	ErrScanner = errors.New("scanner error")
+)
