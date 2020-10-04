369e1813ab76b056498f7e4255a36d27d5556af2
Patrick O'Grady <prohb125@gmail.com>
2020-10-04 13:12:52 -0700

    Add tests for parsing workflow name and concurrency

--- constructor/dsl/errors.go
+++ constructor/dsl/errors.go
@@ -2,14 +2,32 @@ package dsl
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/fatih/color"
 )
 
 type Error struct {
-	Line int
-	Err  error
+	Line         int
+	LineContents string
+	Err          error
+}
+
+func (e *Error) Log() {
+	message := fmt.Sprintf("PARSING FAILED!\nMessage: %s\n\n", e.Err.Error())
+
+	if e.Line > 0 {
+		message = fmt.Sprintf("%sLine: %d\nLine Contents:%s\n\n", message, e.Line, e.LineContents)
+	}
+
+	color.Red(message)
 }
 
 var (
 	ErrEOF     = errors.New("reached end of file")
 	ErrScanner = errors.New("scanner error")
+
+	ErrSyntax                     = errors.New("incorrect syntax")
+	ErrParsingWorkflowName        = errors.New("cannot parse workflow name")
+	ErrParsingWorkflowConcurrency = errors.New("cannot parse workflow concurrency")
 )
