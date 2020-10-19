d23475e3c08129638293965b34a23e2ada10f132
Patrick O'Grady <prohb125@gmail.com>
2020-10-19 16:45:38 -0700

    Pass parser tests

--- parser/exemptions.go
+++ parser/exemptions.go
@@ -1,12 +1,14 @@
 package parser
 
 import (
+	"math/big"
+
 	"github.com/coinbase/rosetta-sdk-go/types"
 )
 
-// FindExemptions returns all matching *types.BalanceExemption
+// findExemptions returns all matching *types.BalanceExemption
 // for a particular *types.AccountIdentifier and *types.Currency.
-func (p *Parser) FindExemptions(
+func (p *Parser) findExemptions(
 	account *types.AccountIdentifier,
 	currency *types.Currency,
 ) []*types.BalanceExemption {
@@ -25,4 +27,36 @@ func (p *Parser) FindExemptions(
 	}
 
 	return matches
+}
+
+// MatchBalanceExemption returns a *types.BalanceExemption
+// associated with the *types.AccountIdentifier, *types.Currency,
+// and difference, if it exists.
+func (p *Parser) MatchBalanceExemption(
+	account *types.AccountIdentifier,
+	currency *types.Currency,
+	difference string, // live - computed
+) *types.BalanceExemption {
+	exemptions := p.findExemptions(account, currency)
+	if len(exemptions) == 0 {
+		return nil
+	}
+
+	// This should never error because we check for validity
+	// before calling this method.
+	bigDifference, ok := new(big.Int).SetString(difference, 10)
+	if !ok {
+		return nil
+	}
+
+	// Check if the reconciliation was exempt (supports compound exemptions)
+	for _, exemption := range exemptions {
+		if exemption.ExemptionType == types.BalanceDynamic ||
+			(exemption.ExemptionType == types.BalanceGreaterOrEqual && bigDifference.Sign() >= 0) ||
+			(exemption.ExemptionType == types.BalanceLessOrEqual && bigDifference.Sign() <= 0) {
+			return exemption
+		}
+	}
+
+	return nil
 }
--- parser/exemptions_test.go
+++ parser/exemptions_test.go
@@ -182,7 +182,268 @@ func TestFindExemptions(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			parser := New(nil, nil, test.balanceExemptions)
-			assert.Equal(t, test.expected, parser.FindExemptions(test.account, test.currency))
+			assert.Equal(t, test.expected, parser.findExemptions(test.account, test.currency))
+		})
+	}
+}
+
+func TestMatchBalanceExemption(t *testing.T) {
+	tests := map[string]struct {
+		balanceExemptions []*types.BalanceExemption
+		account           *types.AccountIdentifier
+		currency          *types.Currency
+		difference        string
+		expected          *types.BalanceExemption
+	}{
+		"no exemptions": {
+			account: &types.AccountIdentifier{
+				Address: "test",
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+		},
+		"no matching exemption": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceDynamic,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 7,
+					},
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+		},
+		"no matching exemptions": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceDynamic,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 7,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "blah",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+		},
+		"currency match": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceDynamic,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 8,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "blah",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+			expected: &types.BalanceExemption{
+				ExemptionType: types.BalanceDynamic,
+				Currency: &types.Currency{
+					Symbol:   "BTC",
+					Decimals: 8,
+				},
+			},
+		},
+		"currency match, wrong sign": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceLessOrEqual,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 8,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "blah",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+		},
+		"currency match, right sign": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceGreaterOrEqual,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 8,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "blah",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+			expected: &types.BalanceExemption{
+				ExemptionType: types.BalanceGreaterOrEqual,
+				Currency: &types.Currency{
+					Symbol:   "BTC",
+					Decimals: 8,
+				},
+			},
+		},
+		"currency match, zero": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceGreaterOrEqual,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 8,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "blah",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "0",
+			expected: &types.BalanceExemption{
+				ExemptionType: types.BalanceGreaterOrEqual,
+				Currency: &types.Currency{
+					Symbol:   "BTC",
+					Decimals: 8,
+				},
+			},
+		},
+		"subaccount match": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceDynamic,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 7,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "hello",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+			expected: &types.BalanceExemption{
+				ExemptionType:     types.BalanceDynamic,
+				SubAccountAddress: stringPointer("hello"),
+			},
+		},
+		"multiple match": {
+			balanceExemptions: []*types.BalanceExemption{
+				{
+					ExemptionType: types.BalanceDynamic,
+					Currency: &types.Currency{
+						Symbol:   "BTC",
+						Decimals: 8,
+					},
+				},
+				{
+					ExemptionType:     types.BalanceDynamic,
+					SubAccountAddress: stringPointer("hello"),
+				},
+			},
+			account: &types.AccountIdentifier{
+				Address: "test",
+				SubAccount: &types.SubAccountIdentifier{
+					Address: "hello",
+				},
+			},
+			currency: &types.Currency{
+				Symbol:   "BTC",
+				Decimals: 8,
+			},
+			difference: "100",
+			expected: &types.BalanceExemption{
+				ExemptionType: types.BalanceDynamic,
+				Currency: &types.Currency{
+					Symbol:   "BTC",
+					Decimals: 8,
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			parser := New(nil, nil, test.balanceExemptions)
+			assert.Equal(t, test.expected, parser.MatchBalanceExemption(test.account, test.currency, test.difference))
 		})
 	}
 }
