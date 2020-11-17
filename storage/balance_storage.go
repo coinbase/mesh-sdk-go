315b4c80329e0d32b6277cb56737a0258aab43a9
Patrick O'Grady <prohb125@gmail.com>
2020-11-17 13:47:13 -0800

    simplify count

--- storage/balance_storage.go
+++ storage/balance_storage.go
@@ -249,18 +249,6 @@ func (b *BalanceStorage) SetBalance(
 		return err
 	}
 
-	key := GetAccountKey(accountNamespace, account, amount.Currency)
-	exists, _, err := dbTransaction.Get(ctx, key)
-	if err != nil {
-		return err
-	}
-
-	if !exists {
-		if err := b.IncrementCounter(ctx, dbTransaction, totalEntries); err != nil {
-			return err
-		}
-	}
-
 	serialAcc, err := b.db.Encoder().Encode(historicalBalanceNamespace, accountEntry{
 		Account:  account,
 		Currency: amount.Currency,
@@ -269,7 +257,12 @@ func (b *BalanceStorage) SetBalance(
 		return err
 	}
 
+	if err := b.IncrementCounter(ctx, dbTransaction, totalEntries); err != nil {
+		return err
+	}
+
 	// Set current record
+	key := GetAccountKey(accountNamespace, account, amount.Currency)
 	if err := dbTransaction.Set(ctx, key, serialAcc, true); err != nil {
 		return err
 	}
