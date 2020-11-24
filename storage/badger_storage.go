5bb19adf72736882f6300dbce5c0b30eeb359fb7
Patrick O'Grady <prohb125@gmail.com>
2020-11-23 16:31:56 -0800

    Adjust performance options

--- storage/badger_storage.go
+++ storage/badger_storage.go
@@ -171,7 +171,7 @@ func PerformanceBadgerOptions(dir string) badger.Options {
 	// This option will have a significant effect the memory. If the level is kept
 	// in-memory, read are faster but the tables will be kept in memory. By default,
 	// this is set to false.
-	opts.KeepL0InMemory = false
+	opts.KeepL0InMemory = true
 
 	// We don't compact L0 on close as this can greatly delay shutdown time.
 	opts.CompactL0OnClose = false
@@ -179,7 +179,7 @@ func PerformanceBadgerOptions(dir string) badger.Options {
 	// LoadBloomsOnOpen=false will improve the db startup speed. This is also
 	// a waste to enable with a limited index cache size (as many of the loaded bloom
 	// filters will be immediately discarded from the cache).
-	opts.LoadBloomsOnOpen = false
+	opts.LoadBloomsOnOpen = true
 
 	return opts
 }
