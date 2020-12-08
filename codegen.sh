10a45fcd39ac5d1f745697ee1bb08d7af28450b2
Patrick O'Grady <prohb125@gmail.com>
2020-12-08 11:37:16 -0600

    update rosetta-specifications version

--- codegen.sh
+++ codegen.sh
@@ -53,7 +53,7 @@ done
 rm -rf tmp;
 
 # Download spec file from releases
-ROSETTA_SPEC_VERSION=1.4.8
+ROSETTA_SPEC_VERSION=1.4.9
 curl -L https://github.com/coinbase/rosetta-specifications/releases/download/v${ROSETTA_SPEC_VERSION}/api.json -o api.json;
 
 # Generate client + types code
