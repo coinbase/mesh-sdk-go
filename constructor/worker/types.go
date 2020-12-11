7a06d2e2f878557630453adc947659bd50887c0b
Patrick O'Grady <prohb125@gmail.com>
2020-12-11 09:57:40 -0600

    define get blob worker

--- constructor/worker/types.go
+++ constructor/worker/types.go
@@ -70,6 +70,23 @@ type Helper interface {
 		*types.PublicKey,
 		map[string]interface{},
 	) (*types.AccountIdentifier, map[string]interface{}, error)
+
+	// SetBlob transactionally persists
+	// a key and value.
+	SetBlob(
+		ctx context.Context,
+		dbTx database.Transaction,
+		key string,
+		value string,
+	) error
+
+	// GetBlob transactionally persists
+	// a key and value.
+	GetBlob(
+		ctx context.Context,
+		dbTx database.Transaction,
+		key string,
+	) (bool, string, error)
 }
 
 // Worker processes jobs.
