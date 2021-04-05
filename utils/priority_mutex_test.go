95f808c03397526499f520b1b5ec4edca66aec43
Raghav Sharma <raghav.sharma@coinbase.com>
2021-04-05 13:07:24 -0700

    forgot to commit

--- utils/priority_mutex_test.go
+++ utils/priority_mutex_test.go
@@ -16,17 +16,40 @@ package utils
 
 import (
 	"context"
-	"testing"
-	"time"
-
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/sync/errgroup"
+	"sync"
+	"testing"
 )
+
+// A wrapper around the PriorityMutex being tested to properly assert invariants without the
+// possibility of race conditions. The `lockWg` completes after the low/high queue is appended to
+// but before the blocking `wait` call so we can assert invariants on the size of the queue.
+type wrappedPriorityMutex struct {
+	*PriorityMutex
+	lockWg sync.WaitGroup
+}
+
+func newWrappedPriorityMutex(numLockCalls int) *wrappedPriorityMutex {
+	w := &wrappedPriorityMutex{PriorityMutex: new(PriorityMutex)}
+	w.lockWg.Add(numLockCalls)
+	return w
+}
+
+func (w *wrappedPriorityMutex) Lock(priority bool) {
+	c := w.PriorityMutex.lockInternal(priority)
+	w.lockWg.Done()
+	w.wait(c)
+}
+
+func (w *wrappedPriorityMutex) Wait() {
+	w.lockWg.Wait()
+}
 
 func TestPriorityMutex(t *testing.T) {
 	arr := []bool{}
 	expected := make([]bool, 60)
-	l := new(PriorityMutex)
+	l := newWrappedPriorityMutex(61)
 	g, _ := errgroup.WithContext(context.Background())
 
 	// Lock while adding all locks
@@ -55,14 +78,14 @@ func TestPriorityMutex(t *testing.T) {
 	}
 
 	// Wait for all goroutines to ask for lock
-	time.Sleep(1 * time.Second)
+	l.Wait()
 
 	// Ensure number of expected locks is correct
 	assert.Len(t, l.high, 10)
 	assert.Len(t, l.low, 50)
 
 	l.Unlock()
-	assert.NoError(t, g.Wait())
+	assert.NoError(t, g.Wait()) // Wait for all goroutines to ask for lock
 
 	// Check results array to ensure all of the high priority items processed first,
 	// followed by all of the low priority items.
