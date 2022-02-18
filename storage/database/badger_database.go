1747dd0c1547c36516d17c9d129690869587302d
millken <millken@gmail.com>
2022-02-18 11:15:13 +0800

    fix unable to open DB

--- storage/database/badger_database.go
+++ storage/database/badger_database.go
@@ -160,6 +160,9 @@ func DefaultBadgerOptions(dir string) badger.Options {
 	// Don't cache blocks in memory. All reads should go to disk.
 	opts.BlockCacheSize = DefaultBlockCacheSize
 
+	//https://github.com/dgraph-io/badger/issues/1353
+	opts.Truncate = true
+
 	return opts
 }
 
