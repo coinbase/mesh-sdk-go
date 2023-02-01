392f6689846aa5b5c8cfa0964ed4f4fafcd02c37
jingwei zhuang <jingwei.zhuang@coinbase.com>
2023-01-31 22:27:00 -0800

    fix uncessary context canceled err

--- fetcher/errors.go
+++ fetcher/errors.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/fatih/color"
 
@@ -62,11 +63,16 @@ func (f *Fetcher) RequestFailedError(
 			log.Println(msg)
 		}
 	}
-
-	errForPrint := fmt.Errorf("%s %s: %w%s", message, err.Error(), ErrRequestFailed, f.metaData)
-	color.Red(errForPrint.Error())
+	// if the err is context.Canceled, do not print it
+	// context.Canceled could because of validation succeed, 
+	// print an error in succeed situation will be confused
+	if !strings.Contains(err.Error(), context.Canceled.Error()) {
+		errForPrint := fmt.Errorf("%s %s: %w%s", message, err.Error(), ErrRequestFailed, f.metaData)
+		color.Red(errForPrint.Error())
+	}
+	
 	return &Error{
-		Err:       errForPrint,
+		Err:       err,
 		ClientErr: rosettaErr,
 		Retry: ((rosettaErr != nil && rosettaErr.Retriable) || transientError(err) || f.forceRetry) &&
 			!errors.Is(err, context.Canceled),
