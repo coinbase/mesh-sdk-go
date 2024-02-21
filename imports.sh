fa62d0317b1b48a27c69faf3dd82de2ca9f70d15
jacquescaocb <102247620+jacquescaocb@users.noreply.github.com>
2024-02-21 14:39:03 -0500

    Add a new asserter client with ignoring rosetta spec validation. (#502)
    * Add a new asserter client with ignoring rosetta spec validation.
    
    * Run make gen to fix check gen error
    
    * refactor asserter code
    
    * update copyright from 2020 to 2024.

--- imports.sh
+++ imports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2020 Coinbase, Inc.
+# Copyright 2024 Coinbase, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- server/routers.go
+++ server/routers.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Coinbase, Inc.
+// Copyright 2024 Coinbase, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
