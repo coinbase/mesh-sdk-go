829ec35937d8c1227bfcaebed679f245a886bfb0
potterbm-cb <135353141+potterbm-cb@users.noreply.github.com>
2024-11-27 15:51:36 -0700

    chore: add unit tests to headerforwarder package (#512)
    * add unit tests for HeaderForwarder class
    
    * add unit tests for response_writer
    
    * make add-license
    
    * make format
    
    * fix linting

---
+++ headerforwarder/forwarder_test.go
@@ -0,0 +1,432 @@
+// Copyright 2024 Coinbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package headerforwarder
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var defaultInterestingHeaders = []string{
+	"X-Client-Id",
+	"X-Client-Type",
+}
+
+//
+// Helpers/Mocks
+//
+
+// for whatever reason this isn't allowed to be named mockTransport
+type fakeTransport struct {
+	LastRequest *http.Request
+	response    *http.Response
+	err         error
+}
+
+func (f *fakeTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	f.LastRequest = r
+	return f.response, f.err
+}
+
+func newMockTransport(incomingHeaders http.Header, err error) http.RoundTripper {
+	resp := &http.Response{
+		Header: incomingHeaders,
+	}
+
+	return &fakeTransport{
+		response: resp,
+		err:      err,
+	}
+}
+
+type mockInvoker struct {
+	LastRequestMD metadata.MD
+	responseMD    metadata.MD
+	err           error
+}
+
+func (m *mockInvoker) Invoke(
+	ctx context.Context,
+	method string,
+	req,
+	reply interface{},
+	cc *grpc.ClientConn,
+	opts ...grpc.CallOption,
+) error {
+	m.LastRequestMD, _ = metadata.FromOutgoingContext(ctx)
+
+	// add response metadata to pointer from call option
+	var headerOpt grpc.HeaderCallOption
+	for _, opt := range opts {
+		if o, ok := opt.(grpc.HeaderCallOption); ok {
+			headerOpt = o
+		}
+	}
+
+	for k, v := range m.responseMD {
+		headerOpt.HeaderAddr.Append(k, v...)
+	}
+
+	return m.err
+}
+
+func newMockInvoker(responseMD metadata.MD, err error) *mockInvoker {
+	return &mockInvoker{
+		err:        err,
+		responseMD: responseMD,
+	}
+}
+
+func contextWithID() (context.Context, string) {
+	ctx := ContextWithRosettaID(context.Background())
+	return ctx, RosettaIDFromContext(ctx)
+}
+
+func newRequest(ctx context.Context, header http.Header) *http.Request {
+	req := &http.Request{}
+
+	if header == nil {
+		req.Header = make(http.Header)
+	} else {
+		req.Header = header
+	}
+
+	return req.WithContext(ctx)
+}
+
+type mockHandler struct {
+	LastRequestID string
+}
+
+func (m *mockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m.LastRequestID = RosettaIDFromRequest(r)
+}
+
+//
+// Tests
+//
+
+func TestRoundTrip(t *testing.T) {
+	tests := map[string]struct {
+		interestingHeaders []string
+		// a mock for headers already captured from a rosetta request by the forwarder
+		outgoingHeaders http.Header
+		// a mock for headers coming from a native node response to the forwarder
+		incomingHeaders http.Header
+
+		ExpectedError error
+		// expected headers on the request sent to the native node
+		ExpectedRequestHeaders http.Header
+		// expected headers to be captured from the native node response
+		ExpectedResponseHeaders http.Header
+	}{
+		"calls actual transport": {
+			ExpectedError: nil,
+		},
+
+		"adds outgoing header to request": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+			ExpectedRequestHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+		},
+
+		"adds multiple outgoing headers to request": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id":   {"test"},
+				"X-Client-Type": {"testclient"},
+			},
+			ExpectedRequestHeaders: map[string][]string{
+				"X-Client-Id":   {"test"},
+				"X-Client-Type": {"testclient"},
+			},
+		},
+
+		"adds outgoing header with multiple values to request": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+			ExpectedRequestHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+		},
+
+		"captures headers from response": {
+			incomingHeaders: map[string][]string{
+				"X-Client-Id":  {"test", "test2"},
+				"Content-Type": {"application/json"},
+			},
+			ExpectedResponseHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+		},
+
+		"captures no headers if none are interesting": {
+			interestingHeaders: []string{},
+			incomingHeaders: map[string][]string{
+				"X-Client-Id":  {"test", "test2"},
+				"Content-Type": {"application/json"},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// default values because http Request always have a non-nil Header
+			// We don't need to do this for expected response because it's compared to the
+			// internal headers map in HeaderForwarder, which is nil if no headers are captured
+			if test.ExpectedRequestHeaders == nil {
+				test.ExpectedRequestHeaders = make(http.Header)
+			}
+
+			// for convenience we use default interesting headers
+			if test.interestingHeaders == nil {
+				test.interestingHeaders = defaultInterestingHeaders
+			}
+
+			ctx, requestID := contextWithID()
+			mockTransport := newMockTransport(test.incomingHeaders, nil)
+
+			hf := &HeaderForwarder{
+				interestingHeaders: test.interestingHeaders,
+				incomingHeaders:    make(map[string]http.Header),
+				actualTransport:    mockTransport,
+				outgoingHeaders: map[string]http.Header{
+					requestID: test.outgoingHeaders,
+				},
+			}
+
+			testReq := newRequest(ctx, nil)
+
+			response, err := hf.RoundTrip(testReq)
+			assert.Equal(t, test.ExpectedError, err)
+
+			defer response.Body.Close()
+
+			outgoingReq := mockTransport.(*fakeTransport).LastRequest
+			capturedHeaders, _ := hf.GetResponseHeaders(requestID)
+
+			assert.Equal(t, test.ExpectedRequestHeaders, outgoingReq.Header)
+			assert.Equal(t, test.ExpectedResponseHeaders, capturedHeaders)
+		})
+	}
+}
+
+func TestUnaryClientInterceptor(t *testing.T) {
+	tests := map[string]struct {
+		interestingHeaders []string
+		// a mock for headers already captured from a rosetta request by the forwarder
+		outgoingHeaders http.Header
+		// a mock for headers coming from a native node response to the forwarder
+		incomingMD metadata.MD
+
+		ExpectedError error
+		// expected headers on the request sent to the native node
+		ExpectedRequestMD metadata.MD
+		// expected headers to be captured from the native node response
+		ExpectedResponseHeaders http.Header
+	}{
+		"calls invoker": {},
+
+		"adds outgoing headers to context": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+			ExpectedRequestMD: map[string][]string{
+				"x-client-id": {"test"},
+			},
+		},
+
+		"adds multiple outgoing headers to context": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id":   {"test"},
+				"X-Client-Type": {"testclient"},
+			},
+			ExpectedRequestMD: map[string][]string{
+				"x-client-id":   {"test"},
+				"x-client-type": {"testclient"},
+			},
+		},
+
+		"adds outgoing header with multiple values to context": {
+			outgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+			ExpectedRequestMD: map[string][]string{
+				"x-client-id": {"test", "test2"},
+			},
+		},
+
+		"captures headers from response metadata": {
+			incomingMD: map[string][]string{
+				"x-client-id":  {"test", "test2"},
+				"content-type": {"application/json"},
+			},
+			ExpectedResponseHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+		},
+
+		"captures headers with multiple values from response metadata": {
+			incomingMD: map[string][]string{
+				"x-client-id":  {"test", "test2"},
+				"content-type": {"application/json"},
+			},
+			ExpectedResponseHeaders: map[string][]string{
+				"X-Client-Id": {"test", "test2"},
+			},
+		},
+
+		"captures no headers if none are interesting": {
+			interestingHeaders: []string{},
+			incomingMD: map[string][]string{
+				"X-Client-Id":  {"test", "test2"},
+				"Content-Type": {"application/json"},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// for convenience we use default interesting headers
+			if test.interestingHeaders == nil {
+				test.interestingHeaders = defaultInterestingHeaders
+			}
+
+			ctx, requestID := contextWithID()
+
+			mock := newMockInvoker(test.incomingMD, nil)
+
+			hf := &HeaderForwarder{
+				interestingHeaders: test.interestingHeaders,
+				incomingHeaders:    make(map[string]http.Header),
+				actualTransport:    nil,
+				outgoingHeaders: map[string]http.Header{
+					requestID: test.outgoingHeaders,
+				},
+			}
+
+			err := hf.UnaryClientInterceptor(ctx, "test", nil, nil, nil, mock.Invoke)
+			assert.Nil(t, err)
+
+			outgoingReqMD := mock.LastRequestMD
+			capturedHeaders, _ := hf.GetResponseHeaders(requestID)
+
+			assert.Equal(t, test.ExpectedRequestMD, outgoingReqMD)
+			assert.Equal(t, test.ExpectedResponseHeaders, capturedHeaders)
+		})
+	}
+}
+
+func TestHeaderForwarderHandler(t *testing.T) {
+	tests := map[string]struct {
+		interestingHeaders []string
+		// a mock for headers on the rosetta request
+		requestHeaders http.Header
+
+		// expected headers to be captured from the rosetta request
+		ExpectedOutgoingHeaders http.Header
+	}{
+		"calls next handler": {},
+
+		"adds request ID to context": {},
+
+		"captures request headers": {
+			requestHeaders: map[string][]string{
+				"X-Client-Id":  {"test"},
+				"Content-Type": {"application/json"},
+			},
+			ExpectedOutgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+		},
+
+		"captures multiple request headers": {
+			requestHeaders: map[string][]string{
+				"X-Client-Id":  {"test"},
+				"Content-Type": {"application/json"},
+			},
+			ExpectedOutgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+		},
+
+		"captures request headers with multiple values": {
+			requestHeaders: map[string][]string{
+				"X-Client-Id":  {"test"},
+				"Content-Type": {"application/json"},
+			},
+			ExpectedOutgoingHeaders: map[string][]string{
+				"X-Client-Id": {"test"},
+			},
+		},
+
+		"captures no headers if none are interesting": {
+			interestingHeaders: []string{},
+			requestHeaders: map[string][]string{
+				"X-Client-Id":  {"test"},
+				"Content-Type": {"application/json"},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// default values because http Request always have a non-nil Header
+			if test.ExpectedOutgoingHeaders == nil {
+				test.ExpectedOutgoingHeaders = make(http.Header)
+			}
+
+			if test.interestingHeaders == nil {
+				test.interestingHeaders = defaultInterestingHeaders
+			}
+
+			mockHandler := &mockHandler{}
+			r := &http.Request{
+				Header: test.requestHeaders,
+			}
+			w := httptest.NewRecorder()
+
+			hf := &HeaderForwarder{
+				interestingHeaders: test.interestingHeaders,
+				incomingHeaders:    make(map[string]http.Header),
+				outgoingHeaders:    make(map[string]http.Header),
+				actualTransport:    nil,
+			}
+
+			handler := hf.HeaderForwarderHandler(mockHandler)
+			handler.ServeHTTP(w, r)
+
+			requestID := mockHandler.LastRequestID
+			assert.NotEmpty(t, requestID)
+
+			// Check that the uuid can be parsed. We don't actually care about this
+			// we just care that it's a string that looks like a uuid and this is easy
+			_, err := uuid.Parse(requestID)
+			assert.Nil(t, err)
+
+			assert.Equal(t, test.ExpectedOutgoingHeaders, hf.outgoingHeaders[requestID])
+		})
+	}
+}
---
+++ headerforwarder/response_writer_test.go
@@ -0,0 +1,74 @@
+// Copyright 2024 Coinbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package headerforwarder
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func mockGetter(id string, headers http.Header) func(string) (http.Header, bool) {
+	return func(s string) (http.Header, bool) {
+		if s != id {
+			return nil, false
+		}
+
+		return headers, true
+	}
+}
+
+var mockHeader http.Header = http.Header{
+	"Test-Header": []string{"test-value"},
+}
+
+func TestWriteHeader(t *testing.T) {
+	writer := httptest.NewRecorder()
+	hfrw := NewResponseWriter(writer, "test-id", mockGetter("test-id", mockHeader))
+
+	hfrw.WriteHeader(200)
+
+	result := writer.Result()
+	defer result.Body.Close()
+	assert.Equal(t, 200, result.StatusCode)
+}
+
+func TestWriteHeaderAddsHeaders(t *testing.T) {
+	writer := httptest.NewRecorder()
+	hfrw := NewResponseWriter(writer, "test-id", mockGetter("test-id", mockHeader))
+
+	hfrw.WriteHeader(200)
+
+	result := writer.Result()
+	defer result.Body.Close()
+	assert.Equal(t, 200, result.StatusCode)
+	assert.Equal(t, mockHeader.Values("test-header"), result.Header.Values("test-header"))
+}
+
+// Tests that headers are written if Write is called before WriteHeader
+func TestWriteAddsHeaders(t *testing.T) {
+	writer := httptest.NewRecorder()
+	hfrw := NewResponseWriter(writer, "test-id", mockGetter("test-id", mockHeader))
+
+	_, err := hfrw.Write([]byte("test-bytes"))
+	assert.Nil(t, err)
+
+	result := writer.Result()
+	defer result.Body.Close()
+	assert.Equal(t, 200, result.StatusCode)
+	assert.Equal(t, mockHeader.Values("test-header"), result.Header.Values("test-header"))
+}
