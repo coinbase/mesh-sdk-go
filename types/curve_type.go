6cd961b1d9018a78b2f59e08b131d6fbf47383fd
Rafal Szymanski <rafal.szymanski@coinbase.com>
2025-06-20 18:46:15 -0400

    Update to v1.4.15 mesh-specifications

--- types/curve_type.go
+++ types/curve_type.go
@@ -17,19 +17,24 @@
 package types
 
 // CurveType CurveType is the type of cryptographic curve associated with a PublicKey. * secp256k1:
-// SEC compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) * secp256r1: SEC
-// compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) * edwards25519: `y
-// (255-bits) || x-sign-bit (1-bit)` - `32 bytes` (https://ed25519.cr.yp.to/ed25519-20110926.pdf) *
-// tweedle: 1st pk : Fq.t (32 bytes) || 2nd pk : Fq.t (32 bytes)
+// SEC compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) *
+// secp256k1_bip340: x-only - `32 bytes`  (implicitly even `Y` coord. Secp256k1 compressed keys may
+// be repurposed by dropping the first byte.
+// (https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki#Public_Key_Generation)) *
+// secp256r1: SEC compressed - `33 bytes` (https://secg.org/sec1-v2.pdf#subsubsection.2.3.3) *
+// edwards25519: `y (255-bits) || x-sign-bit (1-bit)` - `32 bytes`
+// (https://ed25519.cr.yp.to/ed25519-20110926.pdf) * tweedle: 1st pk : Fq.t (32 bytes) || 2nd pk :
+// Fq.t (32 bytes)
 // (https://github.com/CodaProtocol/coda/blob/develop/rfcs/0038-rosetta-construction-api.md#marshal-keys)
 // * pallas: `x (255 bits) || y-parity-bit (1-bit) - 32 bytes` (https://github.com/zcash/pasta)
 type CurveType string
 
 // List of CurveType
 const (
-	Secp256k1    CurveType = "secp256k1"
-	Secp256r1    CurveType = "secp256r1"
-	Edwards25519 CurveType = "edwards25519"
-	Tweedle      CurveType = "tweedle"
-	Pallas       CurveType = "pallas"
+	Secp256k1        CurveType = "secp256k1"
+	Secp256k1_BIP340 CurveType = "secp256k1_bip340"
+	Secp256r1        CurveType = "secp256r1"
+	Edwards25519     CurveType = "edwards25519"
+	Tweedle          CurveType = "tweedle"
+	Pallas           CurveType = "pallas"
 )
