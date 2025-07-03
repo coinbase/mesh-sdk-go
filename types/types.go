2e369533623229a405f6fdd8ca25b863450e85c7
rafalio-cb <135294634+rafalio-cb@users.noreply.github.com>
2025-07-03 16:54:29 -0400

    Update mesh-specification version to 1.5.1 (#530)

--- types/types.go
+++ types/types.go
@@ -18,5 +18,5 @@ const (
 	// RosettaAPIVersion is the version of the Rosetta API
 	// specification used to generate code for this release
 	// of the SDK.
-	RosettaAPIVersion = "1.4.15"
+	RosettaAPIVersion = "1.5.1"
 )
