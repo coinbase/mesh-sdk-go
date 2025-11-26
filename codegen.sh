393b260aa13b3c5cc76b0559a9ff37a009760b5a
Jacques Cao <jacques.cao@coinbase.com>
2025-11-26 14:37:44 -0500

    Revert to mesh-specifications v1.5.1 until v1.6.1 api.json is available

--- codegen.sh
+++ codegen.sh
@@ -50,7 +50,7 @@ done
 rm -rf tmp
 
 # Download spec file from releases
-ROSETTA_SPEC_VERSION=1.6.1
+ROSETTA_SPEC_VERSION=1.5.1
 curl -L https://github.com/coinbase/rosetta-specifications/releases/download/v${ROSETTA_SPEC_VERSION}/api.json -o api.json
 
 # Generate client + types code
